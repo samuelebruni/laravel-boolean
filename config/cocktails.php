69e44402c3ef9365e7521aa8a69f029119cfaea3
EsterDahri <esterdahri@gmail.com>
2023-11-23 14:44:10 +0100

    make model controller seeder and migration

---
+++ config/cocktails.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    [
+        "thumb" => "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/old-fashioned-5a4bab5.jpg?resize=768,574",
+        "name" => "Old fashioned",
+        "slug" => "old-fashioned",
+        "price" => "€ 7,99",
+        "alcoholic" => true,
+        "preparation" => "Put sugar in glass. Cover it with dashes of bitters. Add whiskey and stir until sugar dissolves. Add ice, stir again, and serve. (If the barman starts shaking the ingredients or muddling fruit, have your next round at another bar.)",
+    ],
+    [
+        "thumb" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0s8WkIAjiArDffWd1E5ya0_2_k4iShwy8Bg&usqp=CAU",
+        "name" => "Margarita",
+        "slug" => "margarita",
+        "price" => "€ 6,99",
+        "alcoholic" => true,
+        "preparation" => "Since this recipe includes fresh juice, it should be shaken. Serve over ice in a glass with a salted rim.",
+    ],
+    [
+        "thumb" => "https://cdn.ilclubdellericette.it/wp-content/uploads/2021/06/gin-tonic-700x1000.jpg",
+        "name" => "Gin Tonic",
+        "slug" => "gin-tonic",
+        "price" => "€ 10,99",
+        "alcoholic" => true,
+        "preparation" => "Chill your gin in the fridge or freezer first. The colder the gin, the better. Ideally, chill your gin glass in the freezer as well. Fill your gin glass almost completely full with ice. Pour 50ml (a double shot) of gin over the ice. Top up with tonic water. Stir gently again.",
+    ],
+    [
+        "thumb" => "https://it.cocktail.fabbri1905.com/imgpub/2034493/600/0/cosmopolitan.png",
+        "name" => "Cosmopolitan",
+        "slug" => "cosmopolitan",
+        "price" => "€ 5,99",
+        "alcoholic" => true,
+        "preparation" => "Build all ingredients in a shaker tine with ice and shake. Strain into a martini glass and garnish with lime wheel or zest.",
+    ],
+    [
+        "thumb" => "https://blog.giallozafferano.it/atavolacontea/wp-content/uploads/2020/01/RICETTA-mojito.jpg",
+        "name" => "Mojito",
+        "slug" => "mojito",
+        "price" => "€ 8,99",
+        "alcoholic" => true,
+        "preparation" => "Muddle mint into a shaker tin, then add ice and rest of ingredients. Shake to chill and strain into a highball glass with ice. Top with club soda, if desired, and garnish with mint.",
+    ],
+];
