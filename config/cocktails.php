b38865e33ca6d43e9ae3acaa33a73ab5a56f4a75
samuelebruni <samuele_bruni@hotmail.com>
2023-11-24 00:26:45 +0100

    modified some img link files inside the json

--- config/cocktails.php
+++ config/cocktails.php
@@ -2,15 +2,15 @@
 
 return [
     [
-        "thumb" => "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/old-fashioned-5a4bab5.jpg?resize=768,574",
-        "name" => "Old fashioned",
+        "thumb" => "https://img.freepik.com/free-photo/glass-cognac-dark-chocolate_140725-2976.jpg?size=626&ext=jpg&ga=GA1.1.790381546.1700772469&semt=ais",
+        "name" => "Old Fashioned",
         "slug" => "",
         "price" => "€ 7,99",
         "alcoholic" => true,
         "preparation" => "Put sugar in glass. Cover it with dashes of bitters. Add whiskey and stir until sugar dissolves. Add ice, stir again, and serve. (If the barman starts shaking the ingredients or muddling fruit, have your next round at another bar.)",
     ],
     [
-        "thumb" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0s8WkIAjiArDffWd1E5ya0_2_k4iShwy8Bg&usqp=CAU",
+        "thumb" => "https://media.istockphoto.com/id/1187327272/it/foto/margarita-bevanda-popolare-su-sfondo-nero.jpg?s=612x612&w=0&k=20&c=JwG8K1n6YIH5kA-gtMaKx_nGBi3L8EFLB3xB14H_MAk=",
         "name" => "Margarita",
         "slug" => "",
         "price" => "€ 6,99",
@@ -18,7 +18,7 @@ return [
         "preparation" => "Since this recipe includes fresh juice, it should be shaken. Serve over ice in a glass with a salted rim.",
     ],
     [
-        "thumb" => "https://cdn.ilclubdellericette.it/wp-content/uploads/2021/06/gin-tonic-700x1000.jpg",
+        "thumb" => "https://img.freepik.com/free-photo/squeezing-lemon-into-glass-gin-tonic_123827-21540.jpg?w=1380&t=st=1700777975~exp=1700778575~hmac=d5ead346e97dc72d1f094dd52acabb220f90fadeddcba72aca67eb2e790daa5c",
         "name" => "Gin Tonic",
         "slug" => "",
         "price" => "€ 10,99",
@@ -26,7 +26,7 @@ return [
         "preparation" => "Chill your gin in the fridge or freezer first. The colder the gin, the better. Ideally, chill your gin glass in the freezer as well. Fill your gin glass almost completely full with ice. Pour 50ml (a double shot) of gin over the ice. Top up with tonic water. Stir gently again.",
     ],
     [
-        "thumb" => "https://it.cocktail.fabbri1905.com/imgpub/2034493/600/0/cosmopolitan.png",
+        "thumb" => "https://img.freepik.com/free-photo/wineglass-with-red-drink-reflection-surface_23-2147905763.jpg?size=626&ext=jpg&ga=GA1.1.790381546.1700772469&semt=ais",
         "name" => "Cosmopolitan",
         "slug" => "",
         "price" => "€ 5,99",
@@ -34,7 +34,7 @@ return [
         "preparation" => "Build all ingredients in a shaker tine with ice and shake. Strain into a martini glass and garnish with lime wheel or zest.",
     ],
     [
-        "thumb" => "https://blog.giallozafferano.it/atavolacontea/wp-content/uploads/2020/01/RICETTA-mojito.jpg",
+        "thumb" => "https://png.pngtree.com/background/20230228/original/pngtree-pouring-fresh-mojito-cocktail-in-glass-isolated-on-black-background-picture-image_2066556.jpg",
         "name" => "Mojito",
         "slug" => "",
         "price" => "€ 8,99",
@@ -42,7 +42,7 @@ return [
         "preparation" => "Muddle mint into a shaker tin, then add ice and rest of ingredients. Shake to chill and strain into a highball glass with ice. Top with club soda, if desired, and garnish with mint.",
     ],
     [
-        "thumb" => "https://staticcookist.akamaized.net/wp-content/uploads/sites/21/2023/07/moscow-mule-analcolico-still-life-1200x675.jpg",
+        "thumb" => "https://img.freepik.com/free-photo/tequila-sunrise-with-lemon-slice-salt_140725-1026.jpg?size=626&ext=jpg&ga=GA1.1.790381546.1700772469&semt=ais",
         "name" => "Virgin Moscow Mule",
         "slug" => "",
         "price" => "€ 7,99",
@@ -50,7 +50,7 @@ return [
         "preparation" => "Take ginger beer, a fizzy drink with ginger root, with a slightly spicy aftertaste, in this case non-alcoholic, and lime juice, to be extracted with a citrus juicer. Everything, is combined in a jug in the right proportions, then mixed and poured into individual glasses, together with ice, can be garnished with a slice of lime and a few leaves of mint, which will give an even more intense and fresh aroma.",
     ],
     [
-        "thumb" => "https://staticcookist.akamaized.net/wp-content/uploads/sites/21/2023/06/Mojito-analcolico-0D6A0077-1200x675.jpg",
+        "thumb" => "https://us.images.westend61.com/0001417797pw/hohes-glas-mit-traditionellem-mojito-cocktail-garniert-mit-getrockneter-limette-und-minze-auf-schwarzem-hintergrund-ADSF01656.jpg",
         "name" => "Virgin Mojito",
         "slug" => "",
         "price" => "€ 7,99",
@@ -58,7 +58,7 @@ return [
         "preparation" => "Muddle mint into a shaker tin, then add ice and rest of ingredients. Shake to chill and strain into a highball glass with ice. Top with club soda, if desired, and garnish with mint. Substitute the rum with tonic water.",
     ],
     [
-        "thumb" => "https://mindfulmocktail.com/wp-content/uploads/2020/11/virgin-cosmopolitan-1.jpg",
+        "thumb" => "https://img.freepik.com/free-photo/red-sangria-with-strawberry-table_140725-5428.jpg?size=626&ext=jpg&ga=GA1.1.790381546.1700772469&semt=ais",
         "name" => "Virgin Cosmopolitan Mocktail",
         "slug" => "",
         "price" => "€ 7,99",
