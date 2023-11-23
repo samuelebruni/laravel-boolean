d11c71becc86e1d3f9a6e1d9eea361b0da7ca452
EsterDahri <esterdahri@gmail.com>
2023-11-23 20:22:51 +0100

    add slug, mocktails and fix typo errors in ingredients

--- config/cocktails.php
+++ config/cocktails.php
@@ -4,7 +4,7 @@ return [
     [
         "thumb" => "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/old-fashioned-5a4bab5.jpg?resize=768,574",
         "name" => "Old fashioned",
-        "slug" => "old-fashioned",
+        "slug" => "",
         "price" => "€ 7,99",
         "alcoholic" => true,
         "preparation" => "Put sugar in glass. Cover it with dashes of bitters. Add whiskey and stir until sugar dissolves. Add ice, stir again, and serve. (If the barman starts shaking the ingredients or muddling fruit, have your next round at another bar.)",
@@ -12,7 +12,7 @@ return [
     [
         "thumb" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0s8WkIAjiArDffWd1E5ya0_2_k4iShwy8Bg&usqp=CAU",
         "name" => "Margarita",
-        "slug" => "margarita",
+        "slug" => "",
         "price" => "€ 6,99",
         "alcoholic" => true,
         "preparation" => "Since this recipe includes fresh juice, it should be shaken. Serve over ice in a glass with a salted rim.",
@@ -20,7 +20,7 @@ return [
     [
         "thumb" => "https://cdn.ilclubdellericette.it/wp-content/uploads/2021/06/gin-tonic-700x1000.jpg",
         "name" => "Gin Tonic",
-        "slug" => "gin-tonic",
+        "slug" => "",
         "price" => "€ 10,99",
         "alcoholic" => true,
         "preparation" => "Chill your gin in the fridge or freezer first. The colder the gin, the better. Ideally, chill your gin glass in the freezer as well. Fill your gin glass almost completely full with ice. Pour 50ml (a double shot) of gin over the ice. Top up with tonic water. Stir gently again.",
@@ -28,7 +28,7 @@ return [
     [
         "thumb" => "https://it.cocktail.fabbri1905.com/imgpub/2034493/600/0/cosmopolitan.png",
         "name" => "Cosmopolitan",
-        "slug" => "cosmopolitan",
+        "slug" => "",
         "price" => "€ 5,99",
         "alcoholic" => true,
         "preparation" => "Build all ingredients in a shaker tine with ice and shake. Strain into a martini glass and garnish with lime wheel or zest.",
@@ -36,9 +36,33 @@ return [
     [
         "thumb" => "https://blog.giallozafferano.it/atavolacontea/wp-content/uploads/2020/01/RICETTA-mojito.jpg",
         "name" => "Mojito",
-        "slug" => "mojito",
+        "slug" => "",
         "price" => "€ 8,99",
         "alcoholic" => true,
         "preparation" => "Muddle mint into a shaker tin, then add ice and rest of ingredients. Shake to chill and strain into a highball glass with ice. Top with club soda, if desired, and garnish with mint.",
     ],
+    [
+        "thumb" => "https://staticcookist.akamaized.net/wp-content/uploads/sites/21/2023/07/moscow-mule-analcolico-still-life-1200x675.jpg",
+        "name" => "Virgin Moscow Mule",
+        "slug" => "",
+        "price" => "€ 7,99",
+        "alcoholic" => false,
+        "preparation" => "Take ginger beer, a fizzy drink with ginger root, with a slightly spicy aftertaste, in this case non-alcoholic, and lime juice, to be extracted with a citrus juicer. Everything, is combined in a jug in the right proportions, then mixed and poured into individual glasses, together with ice, can be garnished with a slice of lime and a few leaves of mint, which will give an even more intense and fresh aroma.",
+    ],
+    [
+        "thumb" => "https://staticcookist.akamaized.net/wp-content/uploads/sites/21/2023/06/Mojito-analcolico-0D6A0077-1200x675.jpg",
+        "name" => "Virgin Mojito",
+        "slug" => "",
+        "price" => "€ 7,99",
+        "alcoholic" => false,
+        "preparation" => "Muddle mint into a shaker tin, then add ice and rest of ingredients. Shake to chill and strain into a highball glass with ice. Top with club soda, if desired, and garnish with mint. Substitute the rum with tonic water.",
+    ],
+    [
+        "thumb" => "https://mindfulmocktail.com/wp-content/uploads/2020/11/virgin-cosmopolitan-1.jpg",
+        "name" => "Virgin Cosmopolitan Mocktail",
+        "slug" => "",
+        "price" => "€ 7,99",
+        "alcoholic" => false,
+        "preparation" => "With a combination of cranberry, lime and orange juice, this easy mocktail also gives you a good dose of your daily Vitamin C requirements. Throw the ingredients in a cocktail shaker or mason jar.",
+    ]
 ];
