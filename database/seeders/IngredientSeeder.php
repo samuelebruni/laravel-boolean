ca8ab3eb91a858db54eacefc93a60602aa2b96b5
samuelebruni <samuele_bruni@hotmail.com>
2023-11-23 15:25:34 +0100

    create migration and seed for ingredients

---
+++ database/seeders/IngredientSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Ingredient;
+use Illuminate\Support\Str;
+
+class IngredientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $ingredients = ['Rum', 'Lime', 'Sugar', 'Mint', 'Tonic Water', 'Ice', 'Vodka', 'Triple Sec', 'Crawnbarry Juice', 'Gin', 'Tequila', 'Wiskhey', 'Bitter', 'Orange Peel'];
+
+        foreach ($ingredients as $ingredient) {
+            $newIngredient = new Ingredient();
+            $newIngredient->name = $ingredient;
+            $newIngredient->slug = Str::slug($newIngredient->name);
+            $newIngredient->save();
+        }
+    }
+}
