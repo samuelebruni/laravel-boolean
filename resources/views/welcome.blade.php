5317e9ebb39df88762195cf0621051baac5cb587
GiuseppeDalessio7 <g.dalessio03gmail.com>
2023-11-24 11:17:07 +0100

    modified_logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,16 @@
 
 <head>
     <meta charset="utf-8">
+    <link rel="icon" type="image/svg+xml" href="https://cdn-icons-png.flaticon.com/512/1493/1493169.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel</title>
+
+    <h1 class="text-center">
+        <a style="text-decoration: none; color: #fdd03b " href="http://127.0.0.1:8000/api/cocktails">Clicca
+            qui per
+            andare all'Api</a>
+    </h1>
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
